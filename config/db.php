d26a49855ade2c4270f6d513861bbc82d966aa2d
joeabiramia <joeabiramia27@gmail.com>
2026-04-12 14:08:02 +0300

    sql port

--- config/db.php
+++ config/db.php
@@ -1,10 +1,11 @@
 <?php
 $host = getenv('DB_HOST') ?: 'localhost';
+$port = getenv('DB_PORT') ?: 3306;
 $dbname = getenv('DB_NAME') ?: 'expiryguard_lite';
 $username = getenv('DB_USER') ?: 'root';
 $password = getenv('DB_PASS') ?: '';
 
-$conn = new mysqli($host, $username, $password, $dbname);
+$conn = new mysqli($host, $username, $password, $dbname, $port);
 
 if ($conn->connect_error) {
     die('Database connection failed: ' . $conn->connect_error);
